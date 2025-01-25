fa18f89519e558beddc579737450e0819ebd8115
Adebola <debolacodes@gmail.com>
2025-01-25 18:45:10 +0100

    25th January 2024

---
+++ javascript/scripts.js
@@ -0,0 +1,20 @@
+
+// Install NodeJS so you can run your JS from Code
+// 1. Variables - Named storage location  let, var,
+// Datatypes - number, strings, object, 
+// Constants - const
+let base = 45
+let height = 25
+const half = 0.5
+let greeting = "GOOD MORNING"
+let username = "ADEBOLA"
+let greetuser = greeting + "," + username
+let people = ["bola", "sola"]
+
+console.log(people[2][1])
+
+console.log(greetuser)
+
+
+
+console.log(base, height, half)
